7cc22edc3fe8c5ca4a8877f41ed37c047c8a70c2
Daniel Kirchner <daniel.kirchner@haw-hamburg.de>
2014-04-05 20:56:05 +0200

    Fixed: Missing AdresseDTO added

---
+++ 1 - Code/GeschaeftspartnerKomponente/DataAccessLayer/DTOs/AdresseDTO.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Util.Common.Interfaces;
+
+namespace ApplicationCore.GeschaeftspartnerKomponente.DataAccessLayer
+{
+    public class AdresseDTO : DTOType<AdresseDTO>, ICanConvertToEntity<Adresse>
+    {
+        public virtual int Id { get; set; }
+        public virtual string Strasse { get; set; }
+        public virtual string Hausnummer { get; set; }
+        public virtual string PLZ { get; set; }
+        public virtual string Wohnort { get; set; }
+        public virtual string Land { get; set; }
+
+        public virtual Adresse ToEntity()
+        {
+            Adresse ad = new Adresse();
+            ad.Id = this.Id;
+            ad.Strasse = this.Strasse;
+            ad.Hausnummer = this.Hausnummer;
+            ad.PLZ = this.PLZ;
+            ad.Wohnort = this.Wohnort;
+            ad.Land = this.Land;
+            return ad;
+        }
+    }
+}

0a2729b52894ff9c45d6f1414c29228e363b2570
Daniel Kirchner <daniel.kirchner@haw-hamburg.de>
2014-04-21 22:28:08 +0200

    BuchhaltungKomponente: set up boilerplate code

---
+++ 1 - Code/BuchhaltungKomponente/AccessLayer/IBuchhaltungServices.cs
@@ -0,0 +1,20 @@
+﻿using ApplicationCore.BuchhaltungKomponente.DataAccessLayer;
+
+namespace ApplicationCore.BuchhaltungKomponente.AccessLayer
+{
+    public interface IBuchhaltungServices
+    {
+        // TODO in Unterbeauftragungskomponente auf Frachtauftragsnummer pruefen
+        void createFrachtabrechnung(ref FrachtabrechnungDTO faDTO);
+
+        //DTO oder Nummer??
+        void bezahleFrachtabrechnung(ref FrachtabrechnungDTO faDTO);
+
+        //DTO oder Nummer??
+        void deleteFrachtabrechnung(int faNr);
+
+        FrachtabrechnungDTO findFrachtabrechnung(int faNr);
+
+
+    }
+}
